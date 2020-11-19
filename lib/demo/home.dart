8ed3e6ffa5cfbbe4167e1d510ed54b1da56faca9
NielJun <769288695@qq.com>
2020-11-19 23:11:28 +0800

    完成上方状态栏的搜索按钮

--- lib/demo/home.dart
+++ lib/demo/home.dart
@@ -9,7 +9,19 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: Text('赤羽'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: '导航栏',
+          onPressed: null,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: null,
+            tooltip: '搜索',
+          )
+        ],
+        title: Text('赤羽应用'),
         elevation: 40.0,
       ),
       body: ListViewDemo(),
