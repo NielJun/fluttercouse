32c4c099dbb597b54fabd04cce6bcb055cde3da1
NielJun <769288695@qq.com>
2020-11-19 23:15:09 +0800

    增加tabbar

--- lib/demo/home.dart
+++ lib/demo/home.dart
@@ -6,25 +6,40 @@ import 'list_view_demo.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: '导航栏',
-          onPressed: null,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: null,
-            tooltip: '搜索',
-          )
-        ],
-        title: Text('赤羽应用'),
-        elevation: 40.0,
-      ),
-      body: ListViewDemo(),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: '导航栏',
+              onPressed: null,
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: null,
+                tooltip: '搜索',
+              )
+            ],
+            title: Text('赤羽应用'),
+            elevation: 40.0,
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              ListViewDemo(),
+              ListViewDemo(),
+              ListViewDemo(),
+            ],
+          ),
+        ));
   }
 }
